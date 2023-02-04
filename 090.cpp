26deea5667324cc227bbdb5835aa37bd08d6a9c3
kartik <kartikawadh2004yadav@gmail.com>
2023-02-04 22:30:24 +0530

    KARTIK

---
+++ 090.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    a = 12;
+    int amount1;
+
+    cout<<"size of int"<<sizeof(a)<<endl;
+    cin>>amount1;
+
+    int amount2;
+    cin>>amount2;
+
+    int sum = amount1 + amount2;
+    cout<<sum;
+
+
+    return 0;
+}
